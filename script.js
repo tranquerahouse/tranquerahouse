aa448a06d2da6607491fad93f9102db3188174e7
Shamus jr <shamusjr@Shamuss-MacBook-Air.local>
2026-07-31 14:14:19 +0800

    Complete gallery, footer, reviews and concierge updates

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Welcome to Tranquera House!");
+const conciergeBtn = document.querySelector(".chat-toggle");
+
+if (conciergeBtn) {
+  conciergeBtn.addEventListener("click", () => {
+    window.open(
+      "https://wa.me/60177270183?text=Hi%20Tranquera%20Concierge!%20I%20have%20a%20question%20about%20my%20stay.",
+      "_blank"
+    );
+  });
+}
